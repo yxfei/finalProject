e39a97d14555372f5ca64d34e3285bfac7115232
Bryan Chu <bryan.p.chu@gmail.com>
2015-12-13 19:12:56 -0500

    working bryan

--- javascript/main.js
+++ javascript/main.js
@@ -1,87 +1,77 @@
-function initMap() {
-  var myLatLng;
+var initMap = (function() {
 
+    var map;
 
-  navigator.geolocation.getCurrentPosition(function(position) {
-    var map = new google.maps.Map(document.getElementById('map'), {
-      center: {lat: -34.397, lng: 150.644},
-      zoom: 14
-    });
-    var infoWindow = new google.maps.InfoWindow({map: map});  
+    function initMap() {
+        var myLatLng;
 
-    var coor = {
-      lat: position.coords.latitude,
-      lng: position.coords.longitude
+        map = new google.maps.Map(document.getElementById('map'), {
+            center: {
+                lat: -34.397,
+                lng: 150.644
+            },
+            zoom: 14
+        });
+        var infoWindow = new google.maps.InfoWindow({
+            map: map
+        });
+
+        navigator.geolocation.getCurrentPosition(function(position) {
+            var coor = {
+                lat: position.coords.latitude,
+                lng: position.coords.longitude
+            }
+            currentLocation = new google.maps.LatLng(
+                coor.lat,
+                coor.lng);
+
+            map.setCenter(currentLocation);
+
+            infoWindow.setPosition(currentLocation);
+            infoWindow.setContent('You are here');
+
+            var service = new google.maps.places.PlacesService(map);
+            service.nearbySearch({
+                location: coor,
+                radius: 500,
+                types: ['atm']
+            }, callback);
+
+        }, function() {
+            browserSupportFlag = false;
+            handleNoGeolocation(browserSupportFlag);
+        });
     }
-      currentLocation = new google.maps.LatLng(
-        coor.lat,
-        coor.lng);
-      map.setCenter(currentLocation);
-      
-      infoWindow.setPosition(currentLocation);
-      infoWindow.setContent('You are here');
-      
-      var service = new google.maps.places.PlacesService(map);
-      service.nearbySearch({
-        location: coor,
-        radius: 500,
-        types: ['atm']
-      }, callback);
-
-    
-//      initMap1(coor,map,infoWindow);
-    }, function() {
-      browserSupportFlag = false;
-      handleNoGeolocation(browserSupportFlag);
-    }); 
-}
 
 
-function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-  infoWindow.setPosition(pos);
-  infoWindow.setContent(browserHasGeolocation ?
-                        'Error: The Geolocation service failed.' :
-                        'Error: Your browser doesn\'t support geolocation.');
-}
-
-//initMap();
-
-
-function initMap1(pyrmont) {
-  
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: pyrmont,
-    zoom: 14
-  });
-
-  infoWindow = new google.maps.InfoWindow({map: map});
-
-  console.log('===========',pyrmont.lng);
-  var service = new google.maps.places.PlacesService(map);
-  service.nearbySearch({
-    location: pyrmont,
-    radius: 500,
-    types: ['atm']
-  }, callback);
-}
-
-function callback(results, status) {
-  if (status === google.maps.places.PlacesServiceStatus.OK) {
-    for (var i = 0; i < results.length; i++) {
-      createMarker(results[i]);
+    function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+        infoWindow.setPosition(pos);
+        infoWindow.setContent(browserHasGeolocation ?
+            'Error: The Geolocation service failed.' :
+            'Error: Your browser doesn\'t support geolocation.');
     }
-  }
-}
 
-function createMarker(place) {
-  var placeLoc = place.geometry.location;
-  var marker = new google.maps.Marker({
-    map: map,
-    position: place.geometry.location
-  });
+    function callback(results, status) {
+        console.log('results', results);
+        console.log('status', status);
+        if (status === google.maps.places.PlacesServiceStatus.OK) {
+            for (var i = 0; i < results.length; i++) {
+                createMarker(results[i]);
+            }
+        }
+    }
 
-  google.maps.event.addListener(marker, 'click', function() {
-    infowindow.setContent(place.name);
-    infowindow.open(map, this);
-  });
-}
+    function createMarker(place) {
+        var placeLoc = place.geometry.location;
+        var marker = new google.maps.Marker({
+            map: map,
+            position: place.geometry.location
+        });
+
+        google.maps.event.addListener(marker, 'click', function() {
+            infowindow.setContent(place.name);
+            infowindow.open(map, this);
+        });
+    }
+    return initMap
+})();
